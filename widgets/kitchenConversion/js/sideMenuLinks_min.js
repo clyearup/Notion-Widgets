82498f9f511f64c5c56198ec969327f4a983cd33
clyearup <cluhrman@sea.yearup.org>
2021-03-29 00:02:41 -0700

    Formatting & file removal

--- widgets/kitchenConversion/js/sideMenuLinks_min.js
+++
@@ -1,1 +0,0 @@
-var sideLinks=[{title:"Recipes",link:"/recipes/index.html"},{title:"Recipe Converter",link:"/recipeconverter.html"},{title:"Kitchen Calculator",link:"/kitchencalculator.html"},{title:"Baking Templates",link:"/bakingtemplates/bakingtemplate.html"},{title:"Tutorials",link:"/howto.html"},{title:"Support",link:"/support.html"}];
